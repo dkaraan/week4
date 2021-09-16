0626813e039be049f3c90e9231f2fb5940a404cc
D K <69293658+dkaraan@users.noreply.github.com>
2021-09-16 03:02:43 +0000

    able to merge two json files into one

--- week4demo/lib/data.js
+++ week4demo/lib/data.js
@@ -3,26 +3,37 @@ import fs from 'fs';
 //path module
 import path from 'path';
 
+
+
 //path will be used to making a file path to data sub directory
 // .cwd = currently working directory. process.cwd will send back the string of the currently working directory.
 //using path.join to merge the directories together
 const dataDirectory = path.join(process.cwd(), "data" );
 
+    //get file path to json file
+    //going to join the data directory path with json file
+
 // create function to return all ids in json files in an array
   //***you do this to get data to use in getStaticPaths() function***
 export function getAllIds(){
+
   //get file path to json file
     //going to join the data directory path with json file
-  const filePath = path.join(dataDirectory, "characters.json" )
+  const filePath1 = path.join(dataDirectory, "json1.json" )
+  const filePath2 = path.join(dataDirectory, "json2.json" )
 
   //read file and save as a string
-  const jsonStringData= fs.readFileSync( filePath, "utf8");
+  const jsonStringData1= fs.readFileSync( filePath1, "utf8");
+  const jsonStringData2= fs.readFileSync( filePath2, "utf8");
 
   //turn the string data from jsonStringData into an json array object
-  const jsonObj = JSON.parse(jsonStringData);
+  const jsonObj1 = JSON.parse(jsonStringData1);
+  const jsonObj2 = JSON.parse(jsonStringData2);
+
+  let main = jsonObj1.concat(jsonObj2)
 
   //use .map() to create a new array from json that uses the id's of each object fromt he json file
-  const returnData = jsonObj.map(item =>{
+  const returnData = main.map(item =>{
       return {
         //next.js wants to see it set up liek this using params
         params : {
@@ -40,20 +51,25 @@ export function getAllIds(){
 export function getSortedList(){
   //get file path to json file
     //going to join the data directory path with json file
-  const filePath = path.join(dataDirectory, "characters.json" )
+  const filePath1 = path.join(dataDirectory, "json1.json" )
+  const filePath2 = path.join(dataDirectory, "json2.json" )
 
   //read file and save as a string
-  const jsonStringData= fs.readFileSync( filePath, "utf8");
+  const jsonStringData1= fs.readFileSync( filePath1, "utf8");
+  const jsonStringData2= fs.readFileSync( filePath2, "utf8");
 
   //turn the string data from jsonStringData into an json array object
-  const jsonObj = JSON.parse(jsonStringData);
+  const jsonObj1 = JSON.parse(jsonStringData1);
+  const jsonObj2 = JSON.parse(jsonStringData2);
+
+  let main = jsonObj1.concat(jsonObj2)
 
   //sort array by name
-  jsonObj.sort(function (a, b){
+  main.sort(function (a, b){
     return a.character.localeCompare(b.character);
   });
 
-  return jsonObj.map(item =>{
+  return main.map(item =>{
     return{
       id: item.id.toString(),
       character: item.character
@@ -65,17 +81,22 @@ export function getSortedList(){
 export async function getData(idRequested){
   //get file path to json file
     //going to join the data directory path with json file
-  const filePath = path.join(dataDirectory, 'characters.json' )
+  const filePath1 = path.join(dataDirectory, "json1.json" )
+  const filePath2 = path.join(dataDirectory, "json2.json" )
 
-  //read file
-  const jsonStringData= fs.readFileSync( filePath, 'utf8');
+  //read file and save as a string
+  const jsonStringData1= fs.readFileSync( filePath1, "utf8");
+  const jsonStringData2= fs.readFileSync( filePath2, "utf8");
 
   //turn the string data from jsonStringData into an json array object
-  const jsonObj = JSON.parse(jsonStringData);
+  const jsonObj1 = JSON.parse(jsonStringData1);
+  const jsonObj2 = JSON.parse(jsonStringData2);
+
+  let main = jsonObj1.concat(jsonObj2)
 
   //find the object value in the array that has the matching id
     //.filter() will return the array with one element
-  const objMatch = jsonObj.filter(obj =>{
+  const objMatch = main.filter(obj =>{
     return obj.id.toString()===idRequested;
     }
   );
